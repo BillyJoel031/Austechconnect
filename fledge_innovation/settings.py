853506bfe3c61ef96b3953eb8a1964ae4bcbac5d
BILLYJOEL\Billy Joel <billyjoel031@gmail.com>
2019-05-30 02:28:39 +1000

    Re-design

--- fledge_innovation/settings.py
+++ fledge_innovation/settings.py
@@ -73,23 +73,23 @@ WSGI_APPLICATION = 'fledge_innovation.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'fledgeinnovations',
-#         'USER': 'root',
-#         'PASSWORD': 'Babycuong1',
-#         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-#         'PORT': '3306',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'aus_techconnect1',
+        'USER': 'Admin',
+        'PASSWORD': 'sensordrive',
+        'HOST': 'aus-techconnect.ckqrzl5seop0.ap-southeast-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
--- users/templates/users/forms.py
+++ users/templates/users/forms.py
@@ -4,8 +4,10 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False)
+    last_name = forms.CharField(max_length=30, required=False)
     email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
